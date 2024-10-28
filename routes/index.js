a90a314b98ebbf0546816742c1cfd390043978f0
NeoBitose <ramadhanalif430@gmail.com>
2024-10-29 01:20:02 +0700

    feat: system controller and add system route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const systemController = require("../controllers/api/v1/systemController");
 
 const Auth = require("./authRoute");
 const User = require("./userRoute");
@@ -8,5 +8,9 @@ const Car = require("./carRoute");
 router.use("/api/v1/auth", Auth);
 router.use("/api/v1/users", User);
 router.use("/api/v1/cars", Car);
+
+router.use("/api/v1/health-check", systemController.healtcheck)
+router.use(systemController.onLost);
+router.use(systemController.onError);
 
 module.exports = router

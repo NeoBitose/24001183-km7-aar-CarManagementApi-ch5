48858e5a18c090d1d4924a6335d53b643800a154
NeoBitose <ramadhanalif430@gmail.com>
2024-10-29 12:25:13 +0700

    feat: add crud data users

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
 
+const { UserController } = require("../controllers/api/v1");
+const { uploader } = require("../middlewares");
+
+router.get("/", UserController.getAllUsers);
+router.get("/:id", UserController.getUserbyId);
+router.post("/", uploader.single("fotoProfil"), UserController.createUser);
+// router.patch("/:id", uploader.single("fotoProfil"), UserController.updateUser);
+// router.delete("/:id", UserController.deleteUser);
+
+
 module.exports = router;

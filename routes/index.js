c5cccf28625de1946c09d335701a85d9d93edbee
NeoBitose <ramadhanalif430@gmail.com>
2024-10-29 17:44:35 +0700

    docs: add open api for swagger documentation

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,13 @@ const { SystemController } = require("../controllers/api/v1");
 const Auth = require("./authRoute");
 const User = require("./userRoute");
 const Car = require("./carRoute");
+const Docs = require("./documentationRouter");
 
 router.use("/api/v1/auth", Auth);
 router.use("/api/v1/users", User);
 router.use("/api/v1/cars", Car);
+
+router.use("/api-docs", Docs)
 
 router.use("/api/v1/health-check", SystemController.healtcheck)
 router.use(SystemController.onLost);
